92913b339b19c3dbe55614561453f412f2da846c
gigigal <osnat.vider@gigmit.com>
2015-03-16 21:56:34 +0100

    in item model add relation to store class - belongs to, accept nested attr

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ActiveRecord::Base
  	accepts_nested_attributes_for :category
  	belongs_to :sub_category
  	accepts_nested_attributes_for :sub_category
-
+ 	belongs_to :store
+ 	accepts_nested_attributes_for :store
  	def matched_subcategories
  		@matched_subcategories = SubCategory.find(category_id = self.category_id)
  	end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20150301201908) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
+
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -62,7 +67,7 @@ ActiveRecord::Schema.define(version: 20150301201908) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
