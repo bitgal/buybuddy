cfc8de2b8581bda3b12fb1e936481d6d3091cf1f
gigigal <osnat.vider@gigmit.com>
2015-04-14 20:54:40 +0200

    add scopes on item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,10 @@ class Item < ActiveRecord::Base
  	validates :store, presence: true
  	validates :price, presence: true
 
- 	
+ 	scope :name, -> (name) { where name: name }
+	scope :category, -> (category) { where category: category}
+	scope :store, -> (store) { where store: store}
+
 
  	def matched_subcategories
  		@matched_subcategories = SubCategory.find(category_id = self.category_id)
