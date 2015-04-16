58bb91cb9ec67378edc6b45ce08f8cf3dfce2f9a
gigigal <osnat.vider@gigmit.com>
2015-04-16 22:49:47 +0200

    fix view index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330074436) do
+ActiveRecord::Schema.define(version: 20150416203707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150330074436) do
     t.integer  "store_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
 
   create_table "stores", force: :cascade do |t|
